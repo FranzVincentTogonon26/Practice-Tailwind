c5298b7fed07b94e5851caa08a2023d62a3496c4
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-01-11 21:17:11 +0800

    Aticle Line

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -224,6 +224,21 @@ function Test() {
     </div>
 
 
+    <article className='article'>
+        <time>Mar 10, 2020</time>
+        <h2>Boost your conversion rate</h2>
+        <p>
+            Nulla dolor velit adipisicing duis excepteur esse in duis nostrud occaecat mollit incididunt deserunt sunt. Ut ut
+            sunt laborum ex occaecat eu tempor labore enim adipisicing minim ad. Est in quis eu dolore occaecat excepteur fugiat
+            dolore nisi aliqua fugiat enim ut cillum. Labore enim duis nostrud eu. Est ut eiusmod consequat irure quis deserunt
+            ex. Enim laboris dolor magna pariatur. Dolor et ad sint voluptate sunt elit mollit officia ad enim sit consectetur
+            enim.
+        </p>
+        <div>
+            <img src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+            <span>Lindsay Walton</span>
+        </div>
+    </article>
        
 
 
